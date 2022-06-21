156ae5187109aa45fbb14212bd062beaf4e58cd1
AlbaTB <73853188+albalp@users.noreply.github.com>
2022-06-21 17:03:51 -0500

    Deleting duplicated package.json and components

--- container/src/App.js
+++ container/src/App.js
@@ -1,45 +1,29 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from "react";
 
-import Modal from 'modal/modal';
-import Navigation from 'header/Navigation';
-import { load } from 'web-component-load' ;
-import styled from 'styled-components';
-import dataAPI from '../api/data';
+import Modal from "modal/modal";
+import Navigation from "header/Navigation";
+import { load } from "web-component-load";
+import dataAPI from "../api/data";
 
 function App() {
-    const Button = styled.button`
-    background: transparent;
-    border-radius: 3px;
-    border: 2px solid palevioletred;
-    color: palevioletred;
-    margin: 0 1em;
-    padding: 0.25em 1em;
-    cursor: pointer;
-    &:hover {
-        background-color: palevioletred;
-        color: white;
+  const refCalendar = useRef();
+  useEffect(() => {
+    if (refCalendar.current) {
+      const calendar = refCalendar.current;
+      calendar.currentDate = new Date();
+      calendar.srcData = dataAPI;
     }
-    `
-    const refCalendar = useRef();
-    useEffect(() => {
-        if (refCalendar.current){
-        const calendar = refCalendar.current;
-        calendar.currentDate = new Date;
-        calendar.srcData = dataAPI;
-    }
-        load('http://localhost:5000');
-    }, []);
+    load("http://localhost:5000");
+  }, []);
 
-return (
+  return (
     <div className="App">
-        <Navigation />
-       <Modal />
-       <Button>Add Activity</Button>
-       <calendar-component ref={refCalendar} />
-       <add-activity-button-component />
+      <Navigation />
+      <Modal />
+      <calendar-component ref={refCalendar} />
+      <add-activity-button-component />
     </div>
-);
-
+  );
 }
 
 export default App;
